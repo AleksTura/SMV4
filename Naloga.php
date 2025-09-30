9fba267e441981ba4cf7d87944245a0519b7a5e8
AnjaZma <anja.zmazek.19722@dijak.sc-celje.si>
2025-09-30 10:17:57 +0200

    css

--- Naloga.php
+++ Naloga.php
@@ -2,20 +2,39 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="izgled.css">
     <title>Naloga</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="nalogadiv">
-        <h1>Ime predmeta iz PB</h1>
-    </div>
-    <div class="nalogadiv">
-        <h3>IME NALOGE</h3>
-        <p>Opis naloge </p><br>
-        <div class="DropFile" id="DropFile">
-            plac za oddajo naloge- drag and drop za datoteke
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-lg-8">
+                <div class="nalogadiv text-center">
+                    <h1>Ime predmeta iz PB</h1>
+                </div>
+                
+                <div class="nalogadiv">
+                    <h3>IME NALOGE</h3>
+                    <p>Opis naloge</p>
+                    
+                    <div class="d-flex justify-content-center">
+                        <div class="DropFile" id="DropFile">
+                            <div class="text-center">
+                                <i class="fas fa-cloud-upload-alt mb-2" style="font-size: 2rem; color: #0d6efd;"></i>
+                                <p class="mb-0">Povleci in spusti datoteko sem</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="DroppedFile" id="DroppedFile">
+                        <i class="fas fa-check-circle me-2"></i>NALOGA ODDANA
+                    </div>
+                    
+                    <button id="OddajButton" class="btn btn-primary" onclick="hide()">ODDAJ</button>
+                </div>
+            </div>
         </div>
-        <div class ="DroppedFile" style="display: none" id="DroppedFile">ZE ODDANA NALOGA</div>
-        <button id="OddajButton" onclick="hide()">ODDAJ</button>
     </div>
+
 </body>
 
 <script>
