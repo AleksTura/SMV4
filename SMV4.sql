8f19e32e6a13430359a68cad1d0f8b74eb84bf99
spelazeme <spela.zeme.19720@dijak.sc-celje.si>
2025-09-30 09:00:50 +0200

    DATABASE

--- SMV4.sql
+++ SMV4.sql
@@ -1,39 +1,42 @@
-CREATE DATABASE SMV4 
-GO 
+CREATE DATABASE SMV4; 
+USE SMV4;
 
 CREATE TABLE Ucitelj (
-    Id_ucitelja INT PRIMARY KEY,
-    Ime VARCHAR(50),
-    Priimek VARCHAR(50),
-    Geslo VARCHAR(255)
+    Id_ucitelja int PRIMARY KEY AUTO_INCREMENT,
+    Ime varchar(50),
+    Priimek varchar(50),
+    Geslo varchar(255)
 );
 
 CREATE TABLE Predmet (
-    Id_predmeta INT PRIMARY KEY,
-    Ime_predmeta VARCHAR(100)
+    Id_predmeta int PRIMARY KEY AUTO_INCREMENT,
+    Ime_predmeta varchar(100)
 );
 
 CREATE TABLE Ucenec (
-    Id_dijaka INT PRIMARY KEY,
-    Ime VARCHAR(50),
-    Priimek VARCHAR(50),
-    Letnik INT,
-    Geslo VARCHAR(100)
+    Id_dijaka int PRIMARY KEY AUTO_INCREMENT,
+    Ime varchar(50),
+    Priimek varchar(50),
+    Letnik int,
+    Geslo varchar(100)
 );
 
 CREATE TABLE Uci_predmet (
-    Id_ucitelja INT,
-    Id_predmeta INT,
+    Id_ucitelja int,
+    Id_predmeta int,
     PRIMARY KEY (Id_ucitelja, Id_predmeta),
     FOREIGN KEY (Id_ucitelja) REFERENCES Ucitelj(Id_ucitelja),
-    FOREIGN KEY (Id_predmeta) REFERENCES Predmet(Id_predmeta)
+    FOREIGN KEY (Id_predmeta) REFERENCES Predmet(Id_predmeta),
+    snov varchar(50)
 );
 
 CREATE TABLE Dij_predmet (
-    Id_dijaka INT,
-    Id_predmeta INT,
-    PRIMARY KEY (Id_dijaka, Id_predmeta),
+    Id_dijaka int,
+    Id_ucitelja int,  
+    Id_predmeta int, 
+    PRIMARY KEY (Id_dijaka, Id_ucitelja, Id_predmeta),  
     FOREIGN KEY (Id_dijaka) REFERENCES Ucenec(Id_dijaka),
-    FOREIGN KEY (Id_predmeta) REFERENCES Predmet(Id_predmeta)
-); 
-
+    FOREIGN KEY (Id_ucitelja, Id_predmeta) REFERENCES Uci_predmet(Id_ucitelja, Id_predmeta),  
+    naloge varchar(50),
+    komentarji varchar(500)
+);
